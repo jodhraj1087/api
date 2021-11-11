7f30f517e5fa3049e1fb8e31154a104682023b91
jodhraj1087 <44052728+jodhraj1087@users.noreply.github.com>
2021-11-11 16:39:37 +0530

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,30 @@
+<?php
+@session_start();
+header('Content-Type: text/html; charset=iso-8859-1');
+
+define("HOST","localhost");
+define("DBNAME","expinc");
+define("USERNAME","encourageindia");
+define("PASS","?fbE[%D^Ton^");  
+define("PORTNUMBER","3306");
+define("TYPE","mysql");
+define('ADMINPAGINATIONLIMIT',20);
+define('PAGINATIONLIMIT',30);
+
+$path='';
+if(isset($_SERVER['HTTPS']))
+{
+    $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+}
+else
+{
+    $protocol = 'http';
+}
+$path="appapis.iamstmartin.com/expinc/appapis/";
+
+$path = $protocol . "://" . $path;
+
+define("PATH",$path);
+date_default_timezone_set('US/Eastern');
+require_once "../lib/pdomanager.php";
+?>
